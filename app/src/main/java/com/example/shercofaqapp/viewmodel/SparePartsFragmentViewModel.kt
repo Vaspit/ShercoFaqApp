3bc35b9f1db159ed31e032d9d2c0d983cbea7c87
Vaspit <74468706+Vaspit@users.noreply.github.com>
2022-05-09 17:40:15 +0300

    SparePartsFragmentVieModel logic finished

--- app/src/main/java/com/example/shercofaqapp/viewmodel/SparePartsFragmentViewModel.kt
+++ app/src/main/java/com/example/shercofaqapp/viewmodel/SparePartsFragmentViewModel.kt
@@ -5,6 +5,11 @@ import androidx.lifecycle.ViewModel
 import com.example.shercofaqapp.model.SparePart
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
+import org.json.JSONException
+import org.json.JSONObject
+import java.lang.Exception
+import kotlin.reflect.typeOf
 
 class SparePartsFragmentViewModel: ViewModel() {
 
@@ -15,19 +20,40 @@ class SparePartsFragmentViewModel: ViewModel() {
     ): ArrayList<SparePart> {
 
         val database = Firebase.database.reference
-        val sparePartsArrayList = arrayListOf<SparePart>()
-        var sparePart = SparePart()
+        val sparePartsArrayList = ArrayList<SparePart>()
 
-        //need to fill an array
-        database.child("parts").child(currentSparePartAddress)
-            .child(currentSparePartType)
-            .child(currentSparePartName)
-            .get().addOnSuccessListener {
-            Log.d("SPARE_PARTS", it.value.toString())
-        }.addOnFailureListener{
-            Log.e("firebase", "Error getting data", it)
+        try {
+            database.child("parts").child(currentSparePartAddress)
+                .child(currentSparePartType)
+//                .child(currentSparePartName)
+                .get().addOnSuccessListener {
+                    val gson = Gson()
+                    val json = JSONObject(gson.toJson(it.value).toString())
+                    Log.d("SPARE_PARTS", json.toString())
+
+                    val sparePartsArray = json.getJSONArray(currentSparePartName)
+//
+                    Log.d("SPARE_PARTS", sparePartsArray.toString())
+
+                    for (i in 0 until sparePartsArray.length()) {
+                        val sparePartJSONObject = sparePartsArray.getJSONObject(i)
+                        val sparePart = SparePart(
+                            sparePartJSONObject.getString("sparePartName"),
+                            sparePartJSONObject.getString("sparePartDescription"),
+                            sparePartJSONObject.getString("sparePartLink"),
+                            sparePartJSONObject.getInt("sparePartImage")
+                        )
+
+                        sparePartsArrayList.add(sparePart)
+                    }
+
+                }.addOnFailureListener{
+                    Log.e("firebase", "Error getting data", it)
+                }
+        } catch (e: JSONException) {
+            e.printStackTrace()
         }
-
+        
         return sparePartsArrayList
     }
 
