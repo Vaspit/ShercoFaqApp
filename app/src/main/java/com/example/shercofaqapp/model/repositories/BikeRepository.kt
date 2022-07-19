00c1487d7d92ee0f158f8ca19772c25b45e9bb94
Petr Golovin <vasvko@mail.ru>
2022-07-19 18:18:13 +0300

    DI implementation has started. Job

--- app/src/main/java/com/example/shercofaqapp/model/repositories/BikeRepository.kt
+++ app/src/main/java/com/example/shercofaqapp/model/repositories/BikeRepository.kt
@@ -1,7 +1,5 @@
 package com.example.shercofaqapp.model.repositories
 
-import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.shercofaqapp.model.Bike
 import com.example.shercofaqapp.utils.*
@@ -9,10 +7,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import java.net.URL
 
 class BikeRepository {
 
